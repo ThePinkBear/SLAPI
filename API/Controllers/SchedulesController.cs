bdbaa20b88ac496daccfa4b585bf0e7a124c5607
Bjorn Noctiluca <bjorn.noctiluca@dormakaba.com>
2023-08-16 13:14:55 +0200

    Add credentials to schedules controller

--- API/Controllers/SchedulesController.cs
+++ API/Controllers/SchedulesController.cs
@@ -10,17 +10,23 @@ namespace TestAPI.Controllers
     private readonly HttpClient _client;
     private readonly string? _url;
     private readonly string? _scheduleUrl;
+    private readonly string? _credentials;
 
     public SchedulesController(HttpClient client, IConfiguration config)
     {
+      var c = new Credentials(config);
       _client = client;
       _url = config.GetValue<string>("ExosUrl");
       _scheduleUrl = config.GetValue<string>("Url:Schedule");
+      _credentials = c.Value;
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Schedule>>> GetSchedules()
     {
+      _client.DefaultRequestHeaders.Authorization = new System.Net.Http
+             .Headers.AuthenticationHeaderValue("Basic", _credentials);
+             
       var schedules = await _client.GetFromJsonAsync<List<Schedule>>($"{_url}{_scheduleUrl}");
 
       return schedules == null ? NotFound() : schedules;
