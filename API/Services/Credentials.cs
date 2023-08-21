11f27c307272a9a72dbb37ed63a2592dfa82ac10
Bjorn Noctiluca <bjorn.noctiluca@dormakaba.com>
2023-08-21 13:57:57 +0200

    Read apikey from registry in Credentials.cs

--- API/Services/Credentials.cs
+++ API/Services/Credentials.cs
@@ -1,15 +1,18 @@
 using System.Text;
+using Microsoft.Win32;
 
 public class Credentials
 {
-  public string Value { get; set; } =""; 
+  public string Value { get; set; } ="";
+  RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Exos", true);
   
   public Credentials(IConfiguration config)
   {
     Value = Convert.ToBase64String
       (
         Encoding.ASCII
-        .GetBytes($"{config.GetValue<string>("User:Name")}:{config.GetValue<string>("User:Password")}")
+        // .GetBytes($"{config.GetValue<string>("User:Name")}:{config.GetValue<string>("User:Password")}")
+        .GetBytes($"{config.GetValue<string>("User:Name")}:{key.GetValue("Password")}")
       );
   }
 }
